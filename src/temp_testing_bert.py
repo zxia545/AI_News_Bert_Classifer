229d271a85495a80bf3d81d22888a44944d745ef
zxia545 <xiaozheyuan72@gmail.com>
2023-07-20 21:48:06 +1200

    Update testing_bert

--- src/temp_testing_bert.py
+++ src/temp_testing_bert.py
@@ -1,9 +1,10 @@
-from transformers import BertTokenizer
+from transformers import BertTokenizer, BertModel
 
-model_path = 'bert-base-cased'
+model_path = '/home/data/zhengyuhu/bert/bert-base-uncased'
 
 # ------- BertTokenizer ------
 tokenizer = BertTokenizer.from_pretrained(model_path)
+model = BertModel.from_pretrained(model_path)
 example_text = 'I will watch Memento tonight'
 bert_input = tokenizer(example_text,padding='max_length', 
                        max_length = 10, 
@@ -13,3 +14,10 @@ bert_input = tokenizer(example_text,padding='max_length',
 print(bert_input['input_ids'])
 print(bert_input['token_type_ids'])
 print(bert_input['attention_mask'])
+
+#--------- bert_output -----------
+example_text = tokenizer.decode(bert_input.input_ids[0])
+print(example_text)
+
+output = model(**bert_input)
+print(output)
